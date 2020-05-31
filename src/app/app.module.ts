73dc02db6c30b495d98bf68801d30a338d18ad69
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-31 17:23:35 +0200

    Time to Practice - Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AngularFireModule} from "@angular/fire";
-import {AngularFirestoreModule} from "@angular/fire/firestore";
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import {StopTrainingComponent} from "./training/current-training/stop-training.component";
 import {AuthService} from "./auth/auth.service";
 import {TrainingService} from "./training/training.service";
 import {environment} from "../environments/environment";
-import {FirebaseDatePipe} from "./training/past-trainings/firebase-date.pipe";
 import {AngularFireAuthModule} from "@angular/fire/auth";
 import {UIService} from "./shared/ui.service";
 import {AuthModule} from "./auth/auth.module";
+import {TrainingModule} from "./training/training.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent,
-    FirebaseDatePipe
   ],
   imports: [
     BrowserModule,
@@ -44,12 +31,11 @@ import {AuthModule} from "./auth/auth.module";
     AppRoutingModule,
     FlexLayoutModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     AngularFireAuthModule,
-    AuthModule
+    AuthModule,
+    TrainingModule
   ],
   providers: [AuthService, TrainingService, UIService],
-  bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,9 +1,34 @@
 import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {TrainingComponent} from "./training.component";
+import {CurrentTrainingComponent} from "./current-training/current-training.component";
+import {NewTrainingComponent} from "./new-training/new-training.component";
+import {PastTrainingsComponent} from "./past-trainings/past-trainings.component";
+import {StopTrainingComponent} from "./current-training/stop-training.component";
+import {MaterialModule} from "../material.module";
+import {FirebaseDatePipe} from "./past-trainings/firebase-date.pipe";
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: []
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    StopTrainingComponent,
+    FirebaseDatePipe
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AngularFirestoreModule,
+    FormsModule
+  ],
+  exports: [],
+  entryComponents: [StopTrainingComponent]
 })
 export class TrainingModule {
 
