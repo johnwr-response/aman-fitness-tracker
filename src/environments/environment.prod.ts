1dea5e683ab0ef0136c21c9adf0227c49e5ad13a
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-31 18:18:01 +0200

    Wrap Up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAz4_hiXoCn-w2RRZq78P0MHiefXXjXWdo",
+    authDomain: "aman-fitness-tracker.firebaseapp.com",
+    databaseURL: "https://aman-fitness-tracker.firebaseio.com",
+    projectId: "aman-fitness-tracker",
+    storageBucket: "aman-fitness-tracker.appspot.com",
+    messagingSenderId: "1084962221546",
+    appId: "1:1084962221546:web:f3332f601bd7224ff45693"
+  }
 };
